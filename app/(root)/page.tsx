b0b7fc958e1183fc18bf531ca17f9ef5ed102e50
AGUNAN <agunan11@gmail.com>
2025-06-23 07:40:39 +0530

    add feedback

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,26 +1,26 @@
-import React from "react";
 import Link from "next/link";
 import Image from "next/image";
+
 import { Button } from "@/components/ui/button";
 import InterviewCard from "@/components/InterviewCard";
-import { getCurrentUser } from "@/lib/actions/auth.action";
-import { dummyInterviews } from "@/constants";
-// import {
-//   getInterviewsByUserId,
-//   getLatestInterviews,
-// } from "@/lib/actions/general.action";
 
-const page = async () => {
+import { getCurrentUser } from "@/lib/actions/auth.action";
+import {
+  getInterviewsByUserId,
+  getLatestInterviews,
+} from "@/lib/actions/general.action";
+
+async function Home() {
   const user = await getCurrentUser();
 
   // Fetch data safely using guaranteed user.id
-  // const [userInterviews, allInterview] = await Promise.all([
-  //   getInterviewsByUserId(user!.id),
-  //   getLatestInterviews({ userId: user!.id }),
-  // ]);
+  const [userInterviews, allInterview] = await Promise.all([
+    getInterviewsByUserId(user!.id),
+    getLatestInterviews({ userId: user!.id }),
+  ]);
 
-  // const hasPastInterviews = userInterviews && userInterviews.length > 0;
-  // const hasUpcomingInterviews = allInterview && allInterview.length > 0;
+  const hasPastInterviews = userInterviews && userInterviews.length > 0;
+  const hasUpcomingInterviews = allInterview && allInterview.length > 0;
   return (
     <>
       <section className="card-cta">
@@ -48,18 +48,21 @@ const page = async () => {
         <h2>Your Interviews</h2>
 
         <div className="interviews-section">
-          {dummyInterviews.map((interview) => (
-            <InterviewCard
-              key={interview.id}
-              userId={user?.id}
-              interviewId={interview.id}
-              role={interview.role}
-              type={interview.type}
-              techstack={interview.techstack}
-              createdAt={interview.createdAt}
-            />
-          ))}
-          {/* <p>You haven&apos;t taken any interviews yet</p> */}
+          {hasPastInterviews ? (
+            userInterviews?.map((interview) => (
+              <InterviewCard
+                key={interview.id}
+                userId={user?.id}
+                interviewId={interview.id}
+                role={interview.role}
+                type={interview.type}
+                techstack={interview.techstack}
+                createdAt={interview.createdAt}
+              />
+            ))
+          ) : (
+            <p>You haven&apos;t taken any interviews yet</p>
+          )}
         </div>
       </section>
 
@@ -67,22 +70,25 @@ const page = async () => {
         <h2>Take Interviews</h2>
 
         <div className="interviews-section">
-          {dummyInterviews.map((interview) => (
-            <InterviewCard
-              key={interview.id}
-              userId={user?.id}
-              interviewId={interview.id}
-              role={interview.role}
-              type={interview.type}
-              techstack={interview.techstack}
-              createdAt={interview.createdAt}
-            />
-          ))}
-          {/* <p>There are no interviews available</p> */}
+          {hasUpcomingInterviews ? (
+            allInterview?.map((interview) => (
+              <InterviewCard
+                key={interview.id}
+                userId={user?.id}
+                interviewId={interview.id}
+                role={interview.role}
+                type={interview.type}
+                techstack={interview.techstack}
+                createdAt={interview.createdAt}
+              />
+            ))
+          ) : (
+            <p>There are no interviews available</p>
+          )}
         </div>
       </section>
     </>
   );
-};
+}
 
-export default page;
+export default Home;
